a9e027b48f164f2b03ed5973fb16acf56fef15c8
joseramon23 <joseportillo806@hotmail.com>
2024-04-02 09:38:40 +0200

    fix the creation of the User class

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,6 @@
-const User = require('../models/user.model.js') 
-const { validatePassword, validateUser } = require('../utils/validateUser.js')
+const UserModel = require('../models/user.model.js')
+const User = new UserModel 
+const { validatePassword, validateUser, validateIsEmail } = require('../utils/validate.js')
 const bcrypt = require('bcrypt') 
 const jwt = require('jsonwebtoken') 
 
