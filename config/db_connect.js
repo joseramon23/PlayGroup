01951513be06a11326637f52448baf683bfa0850
joseramon23 <joseportillo806@hotmail.com>
2024-04-02 15:56:20 +0200

    add new environment variable and new database for testing

--- config/db_connect.js
+++ config/db_connect.js
@@ -3,11 +3,17 @@ const { config } = require('dotenv')
 
 config()
 
+const databaseName = process.env.NODE_ENV === "development" ?
+    process.env.MYSQLDB_DATABASE :
+    process.env.MYSQLDB_DATABASE_TEST
+
+console.log(databaseName)
+
 const pool = createPool({
     host: process.env.MYSQLDB_HOST,
     user: process.env.MYSQLDB_USER,
     password: process.env.MYSQLDB_PASSWORD,
-    database: process.env.MYSQLDB_DATABASE,
+    database: databaseName,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
