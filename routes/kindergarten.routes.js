53733a73fdfdfe93b61c3df45fdd16e807a26450
joseramon23 <joseportillo806@hotmail.com>
2024-04-08 09:49:35 +0200

    add authentication jwt

--- routes/kindergarten.routes.js
+++ routes/kindergarten.routes.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const kindergartenController = require('../controllers/kindergarten.controller')
+const authToken = require('../middlewares/authToken')
 
 const router = express.Router()
 
-router.get('/kindergarten', kindergartenController.getAllKindergarten)
-router.get('/kindergarten/:id', kindergartenController.getKindergarten)
-router.post('/kindergarten', kindergartenController.createKindergarten)
-router.put('/kindergarten/:id', kindergartenController.updateKindergarten)
-router.delete('/kindergarten/:id', kindergartenController.deleteKindergarten)
+router.get('/kindergarten', authToken, kindergartenController.getAllKindergarten)
+router.get('/kindergarten/:id', authToken, kindergartenController.getKindergarten)
+router.post('/kindergarten', authToken, kindergartenController.createKindergarten)
+router.put('/kindergarten/:id', authToken, kindergartenController.updateKindergarten)
+router.delete('/kindergarten/:id', authToken, kindergartenController.deleteKindergarten)
 
 module.exports = router
