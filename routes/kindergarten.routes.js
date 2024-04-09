fa3a240520408021c991546d0d4aafe3ec53cab8
joseramon23 <joseportillo806@hotmail.com>
2024-04-09 10:49:41 +0200

    add new middlewares

--- routes/kindergarten.routes.js
+++ routes/kindergarten.routes.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const kindergartenController = require('../controllers/kindergarten.controller')
 const authToken = require('../middlewares/authToken')
+const authAdmin = require('../middlewares/authAdmin')
 
 const router = express.Router()
 
-router.get('/kindergarten', authToken, kindergartenController.getAllKindergarten)
+router.get('/kindergarten', authToken, authAdmin, kindergartenController.getAllKindergarten)
 router.get('/kindergarten/:id', authToken, kindergartenController.getKindergarten)
 router.post('/kindergarten', authToken, kindergartenController.createKindergarten)
 router.put('/kindergarten/:id', authToken, kindergartenController.updateKindergarten)
-router.delete('/kindergarten/:id', authToken, kindergartenController.deleteKindergarten)
+router.delete('/kindergarten/:id', authToken, authAdmin, kindergartenController.deleteKindergarten)
 
 module.exports = router
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const userController = require('../controllers/user.controller')
+const authToken = require('../middlewares/authToken')
+const authAdmin = require('../middlewares/authAdmin')
+const upload = require('../middlewares/imageUpload')
 
 const router = express.Router()
 
-router.get('/users', userController.getAllUsers)
-router.get('/users/:id', userController.getUser)
-router.post('/users', userController.createUser)
-router.put('/users/:id', userController.updateUser)
-router.put('/users/password/:id', userController.userUpdatePassword)
+router.post('/users/login', userController.loginUser)
+router.post('/users', upload.single('image'), userController.createUser)
+
+router.get('/users', authToken, authAdmin, userController.getAllUsers)
+router.get('/users/:id', authToken, userController.getUser)
+router.put('/users/:id', authToken, upload.single('image'), userController.updateUser)
+router.put('/users/password/:id', authToken, userController.userUpdatePassword)
 
 module.exports = router
