f05b3d4e1c80a748f36f610abee30b9a136fe1f4
joseramon23 <joseportillo806@hotmail.com>
2024-04-18 13:11:27 +0200

    add password validation to update password

--- schemas/users.js
+++ schemas/users.js
@@ -13,9 +13,9 @@ const userSchema = z.object({
         required_error: 'El email es requerido',
         invalid_string_error: 'El email no es válido'
     }),
-    kindergarten_id: z.number().optional().refine(async (id) => await kindergartenExists(id), {
+    kindergarten_id: z.number().refine(async (id) => await kindergartenExists(id), {
         message: 'El kindergarten no existe en la base de datos'
-    }),
+    }).optional(),
     password: z.string().refine(password => passRegex.test(password), {
         message: 'La contraseña debe contener al menos una mayúscula y dos números'
     }),
@@ -26,37 +26,13 @@ const userSchema = z.object({
             invalid_type_error: 'El dato introducido no es válido'
         }
     ).default('profesor')
-}).refine(data => data.password === data.passwordConfirm, {
-        message: 'Las contraseñas deben coincidir',
-        path: ['passwordConfirm']
 })
 
-const userUpdateSchema = z.object({
-    name: z.string({
-        invalid_type_error: 'El nombre debe ser un string',
-        required_error: 'El nombre es requerido'
-    }).optional(),
-    email: z.string().email({
-        invalid_type_error: 'El email debe ser un string',
-        required_error: 'El email es requerido',
-        invalid_string_error: 'El email no es válido'
-    }).optional(),
-    kindergarten_id: z.number().optional(),
-    password: z.string().refine(password => passRegex.test(password), {
+const passwordSchema = z.object({
+    password: z.string().min(1, 'La antigua contraseña es requerida'),
+    newPass: z.string().min(1, 'Debes ingresar una nueva contraseña').refine(password => passRegex.test(password), {
         message: 'La contraseña debe contener al menos una mayúscula y dos números'
-    }).optional(),
-    newPassword: z.string().refine(password => passRegex.test(password), {
-        message: 'La contraseña debe contener al menos una mayúscula y dos números'
-    }).optional(),
-    rol: z.string(
-        z.enum(['profesor', 'director']),
-        {
-            invalid_type_error: 'El dato introducido no es válido'
-        }
-    ).default('profesor').optional()
-}).refine(data => data.password === data.newPassword, {
-    message: 'Las contraseñas deben coincidir',
-    path: ['newPassword']
+    })
 })
 
 const validateUserSchema = (input) => {
@@ -64,10 +40,15 @@ const validateUserSchema = (input) => {
 }
 
 const validatePartialUser = (input) => {
-    return userUpdateSchema.safeParse(input)
+    return userSchema.partial().safeParseAsync(input)
 }
+
+const validatePassUpdate = (input) => {
+    return passwordSchema.safeParse(input)
+} 
 
 module.exports = {
     validateUserSchema,
-    validatePartialUser
+    validatePartialUser,
+    validatePassUpdate
 }
