05daa15296508fa278d4844d14e21995a0ad006d
joseramon23 <joseportillo806@gmail.com>
2024-04-23 12:11:29 +0200

    modify validate kindergarten_id because with form-data it sends a string and with zod we change it to number

--- schemas/student.js
+++ schemas/student.js
@@ -1,16 +1,17 @@
 import z from 'zod'
-import { kindergartenExists } from '../utils/validate'
+import { kindergartenExists } from '../utils/validate.js'
+
+const dateRegex = /^\d{4}-\d{2}-\d{2}$/
 
 const studentSchema = z.object({
     name: z.string({
         invalid_type_error: 'El nombre debe ser un string',
         required_error: 'El nombre es requerido'
     }).min(1, 'El nombre está vacío').max(250),
-    birthdate: z.date({
-        invalid_type_error: 'La fecha de nacimiento debe ser una fecha',
-        required_error: 'La fecha de nacimiento es requreida'
+    birthdate: z.string().refine(date => dateRegex.test(date), {
+        message: 'La fecha introducida no es válida'
     }),
-    kindergarten_id: z.number().refine(async (id) => await kindergartenExists(id), {
+    kindergarten_id: z.number().or(z.string().transform(Number)).refine(async (id) => await kindergartenExists(id), {
         message: 'La guarderia no existe en la base de datos'
     })
 })
