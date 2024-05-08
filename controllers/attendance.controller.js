bb55fb271869aa4cedfc2d9fdc1876e6a631525e
joseramon23 <joseportillo806@gmail.com>
2024-05-08 18:51:18 +0200

    create functions to GET methods

--- controllers/attendance.controller.js
+++ controllers/attendance.controller.js
@@ -1,45 +1,60 @@
-import { AttendanceDiary } from '../models/attendanceDiary.model.js'
+import AttendanceDiary from '../models/attendanceDiary.model.js'
+import Student from '../models/students.model.js'
+import { errorMessage, unauthorizedMessage } from '../utils/errorHandler.js'
 
-const attendanceDiary = new AttendanceDiary()
 
-// TODO comentar y probar las funciones
-export const getAllByStudent = async (req, res) => {
+/**
+ * Retrieves all attendance records based on the provided filters.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A promise that resolves when the operation is complete.
+ */
+export const getAllAttendance = async (req, res) => {
+    const filters = {
+        kindergarten: req.query.kindergarten,
+        student: req.query.student
+    }
+
     try {
-        const id = req.params.id
-        const result = await attendanceDiary.getAllByStudent(id)
-        res.status(200).json(result)
+        if (!filters.kindergarten && !filters.student) {
+            if (req.user.rol !== 'webadmin') {
+                return res.status(500).json(unauthorizedMessage());
+            }
+        }
+
+        if (filters.kindergarten) {
+            if (!(req.user.kindergarten_id === Number(filters.kindergarten) || req.user.rol === 'webadmin')) {
+                return res.status(401).json(unauthorizedMessage('No estás autorizado para acceder a esta guardería'));
+            }
+        }
+
+        if (filters.student) {
+            const student = await Student.getId(filters.student);
+            if (!(req.user.kindergarten_id === student.kindergarten_id || req.user.rol === 'webadmin')) {
+                return res.status(401).json(unauthorizedMessage('No estás autorizado para acceder a este estudiante'));
+            }
+        }
+
+        const result = await AttendanceDiary.getAll(filters);
+        res.status(200).json({
+            success: true,
+            statusCode: 200,
+            statusMessage: 'Accepted',
+            data: result
+        });
     } catch (error) {
-        res.status(500).json({ message: error.message })
+        res.status(500).json(errorMessage(`Error al obtener entrada: ${error.message}`));
     }
 }
 
 export const getAttendanceDate = async (req, res) => {
-    try {
-        const date = req.params.date
-        const result = await attendanceDiary.getAttendanceDate(date)
-        res.status(200).json(result)
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
+    //TODO
 }
 
 export const setEntrance = async (req, res) => {
-    try {
-        const data = req.body
-        const result = await attendanceDiary.setEntrance(data)
-        res.status(200).json(result)
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
+    //TODO
 }
 
 export const setExit = async (req, res) => {
-    try {
-        const id = req.params.id
-        const data = req.body
-        const result = await attendanceDiary.setExit(id, data)
-        res.status(200).json(result)
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
+    //TODO
 }
